11bb8307182b2cdb8f344b3d415205ca40c85ec5
Brad Reardon <brad.jay.reardon@gmail.com>
2018-04-23 18:10:48 -0400

    Various updates to provisioning config

--- swaprcli/config.py
+++ swaprcli/config.py
@@ -2,5 +2,9 @@ PRODUCTION_DOCKER_IMAGES = {
     'backend': {
         'context': 'gatech-swapr-server-node',
         'dockerfile': 'gatech-swapr-server-node/Dockerfile'
+    },
+    'www': {
+        'context': 'swapr-react-client',
+        'dockerfile': 'swapr-react-client/Dockerfile.build'
     }
 }
